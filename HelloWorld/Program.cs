2058b5ef2717d03a16378695c950ca5b0f1487d1
Chris <55262451+ChristopherHoganJr@users.noreply.github.com>
2023-08-20 23:48:37 -0700

    old way of writing

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,8 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# Game Devs!");
-Console.Write("This is 2nd line of text.");
-Console.Write(" This is 3rd write statement.");
-Console.WriteLine("This is the 4th line of text.");
-Console.WriteLine("This is the 5th line of text.");
+﻿using System;
 
-Console.WriteLine("This is the 6th line of text.");
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
