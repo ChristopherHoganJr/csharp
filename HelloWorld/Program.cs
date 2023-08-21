ab80f27c5eeb0e2eba45ff553d6e676baed9ce72
Chris <55262451+ChristopherHoganJr@users.noreply.github.com>
2023-08-20 23:40:25 -0700

    Console.WriteLine();

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, C# Game Devs!");
+Console.Write("This is 2nd line of text.");
+Console.Write(" This is 3rd write statement.");
+Console.WriteLine("This is the 4th line of text.");
+Console.WriteLine("This is the 5th line of text.");
+
+Console.WriteLine("This is the 6th line of text.");
